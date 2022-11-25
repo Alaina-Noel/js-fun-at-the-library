6822696a9f19053004720c8b30f69127ab19417a
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-25 13:18:13 -0800

    iteration 1 complete

--- src/book.js
+++ src/book.js
@@ -1,12 +1,39 @@
-function createTitle() {
-  
+function createTitle(string) {
+  return `The ${string}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name: name, age: age, pronouns: pronouns};
+}
+
+function saveReview(details, reviews) {
+  if (reviews.includes(details) === false) {
+  return reviews.push(details);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+          title,
+          mainCharacter,
+          pageCount: calculatePageCount(title),
+          genre
+        };
+}
+
+function editBook(bookTitle) {
+  return bookTitle.pageCount = bookTitle.pageCount * 3 / 4;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
