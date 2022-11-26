d59dc61fe85f32e03cb5ff38afae3b41f3f2f338
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-25 17:56:47 -0800

    it 2 complete

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,29 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  const hasSameTitle = (element) => element.title === bookTitle;
+  indexOfBook = shelf.findIndex(hasSameTitle);
+  shelf.splice(indexOfBook, 1);
+  return shelf;
+}
+
+function listTitles(shelf) {
+  return shelf.map(book => book.title).join(', ');
+}
+
+function searchShelf(shelf, bookTitle) {
+  const hasSameTitle = (element) => element.title === bookTitle;
+  indexOfBook = shelf.findIndex(hasSameTitle);
+  return indexOfBook >= 0;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
