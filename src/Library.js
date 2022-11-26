e0b4708ba5fcc33034bdc540463f4dd5355ef687
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-25 18:29:59 -0800

    added book to shelf at library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,14 @@
+function createLibrary(name) {
+  return {name, shelves: {fiction: [], fantasy: [], nonFiction: []}};
+}
 
+function addBook(library, book) {
+  genreKey = `${book.genre}`;
+  return library.shelves[genreKey].push(book);
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
